aef11187800536e9736a4cb548cdf58557e58edc
Gayatri Lakshmi Jonna <gayatri.jonna@qentelli.com>
2022-01-27 13:17:03 +0530

    intial post route

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-// Importing express
+// Importing expressjs for creating the server and creating api routes
 const express = require('express')
+
+// Body parser for getting the data through the url
+const bodyParser = require('body-parser')
+
+// Const
+const { request } = require('express')
 const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.post('/quotes',(req,res) =>{
+    res.send(req.body)
+})
 
 app.get('/',(req,res) => {
 
-     res.send('Hello World')
+     res.sendFile(__dirname + './index.html')
 
 })
 
-const PORT = 3000
+
+const PORT = 5000
+
 app.listen(PORT,() => {
-    console.log('server running at port ${PORT}')
+    console.log(`server running at port ${PORT}`)
 })
