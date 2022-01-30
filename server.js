6a23b6fc338098be56453e2c2b2f6ad1fe8e9653
Gayatri Lakshmi Jonna <gayatri.jonna@qentelli.com>
2022-01-30 13:11:02 +0530

    Updating NodecrudApi

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const bodyParser = require('body-parser')
 
 // Importing MongoClient 
 const MongoClient = require('mongodb').MongoClient
+const res = require('express/lib/response')
+const {locals} = require('ejs')
 
 // Creating app function from the express functional constructor to use it for creating server and apis
 const app = express()
 
 // Enabling body parser with urlencoded form data to be true
 app.use(bodyParser.urlencoded({extended:true}))
+
+// Setting ejs template engine
+app.set('view engine','ejs')
 
 // Database Connection String
 const connectionString = "mongodb+srv://gayathri:gayathri3mongo@cluster0.p0dgw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
@@ -21,11 +26,10 @@ MongoClient.connect(connectionString,{useUnifiedTopology:true})
 .then(client => { 
     console.log('connected to database')
 
-    // Connecting the database
-    const db = client.db('star-war-quotes')
-
-    // Connecting the collection
-    const quotesCollection = db.collection('quotes')
+        // Connecting the database
+        const db = client.db('star-war-quotes')
+        // Connecting the collection
+        const quotesCollection = db.collection('quotes')
 
 
     // Create with POST
@@ -50,12 +54,29 @@ MongoClient.connect(connectionString,{useUnifiedTopology:true})
           db.collection('quotes').find().toArray()
            // Waiting for the promise to send us the result back
            .then(result=>{
-                res.send(result)
+                res.render('index.ejs',{quotes:result})
             })
-        // Waiting for the promise to send us the error back
-        .catch(error=>console.error(error))
+          // Waiting for the promise to send us the error back
+          .catch(error=>console.error(error))
     })
-}).catch(console.error)
+
+     //3.Updating data
+     app.put('/updatequote',(req,res)=>{
+
+             quotesCollection.findOneAndUpdate()
+           // Waiting for the promise to send us the result back
+           .then(result=>{
+                res.send(result)
+           })
+          // Waiting for the promise to send us the error back
+         .catch(error=>console.error(error))
+       })
+
+ }).catch(console.error) // MongoDB Atlas Cluster/Server Connection Error
+
+    app.get('/',(req,res)=>{
+        res.sendFile(__dirname + '/index.html')
+    })
 
 // Connecting the server
 const PORT = 5000
